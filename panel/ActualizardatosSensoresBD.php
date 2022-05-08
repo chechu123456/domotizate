48917cd24ff6eb503a516e9ce2543e2a0ca2578c
Sergio <sergyrp98@gmail.com>
2022-05-08 22:49:47 +0200

    Correcciones de errores para usarlo desde el nuevo dominio (domotizate.site)

--- panel/ActualizardatosSensoresBD.php
+++ panel/ActualizardatosSensoresBD.php
@@ -5,7 +5,7 @@
     if(isset($_POST['nombSensor']) || isset($_POST['valor']) ){
         $nombSensor = $_POST['nombSensor'];
         $valor = $_POST['valor'];
-        echo "El sensor: $nombSensor con valor: $valor";
+        echo "El sensor: $nombSensor con valor: $valor de la casa: ". $_SESSION['idCasa'];
         $idCasa = $_SESSION['idCasa'];
         $nickname = $_SESSION['nickname'];
         $listado->crearTienenRegistro($nombSensor, $valor, $idCasa, $nickname);
