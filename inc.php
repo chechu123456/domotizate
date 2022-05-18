9311be71cb65cce3bb5660441a7c0935586f7b91
“Sergio” <sergyrp98@gmail.com>
2022-05-18 12:38:32 +0200

    Revert "Correcciones de errores para usarlo desde el nuevo dominio (domotizate.site)"
    This reverts commit 48917cd24ff6eb503a516e9ce2543e2a0ca2578c.

--- inc.php
+++ inc.php
@@ -3,10 +3,10 @@
     $ruta = localizacion();
     require("bd/consultas.php");
 
-    $db_host = "domotizate.site";
-    $db_user =  "k204701_sergio";
-    $db_passwd = "LjSf&]0PP7$8"; 
-    $db_name = "k204701_domotizate";
+    $db_host = "allnatural.site";
+    $db_user =  "r165651";
+    $db_passwd = "p-MD*zWaEf_v"; 
+    $db_name = "r165651_domotizate";
     $listado = new Listado($db_host, $db_user, $db_passwd, $db_name);
     
     
